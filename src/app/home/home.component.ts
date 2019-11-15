d096963362653cf7123410b66271088d96a56fd9
AleksandrChistov <aleksandrchistov.ru@yandex.ru>
2019-11-15 09:35:59 +0700

    Вывел список жанров к каждому фильму на главной странице

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,11 @@ export interface PopularFilms {
   results?: []
 }
 
+export interface Genres {
+  id: number
+  name: string
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -17,15 +22,50 @@ export interface PopularFilms {
 })
 export class HomeComponent implements OnInit {
 
+  genres: Genres[] = [
+    {id: 28, name: "боевик"},
+    {id: 12, name: "приключения"},
+    {id: 16, name: "мультфильм"},
+    {id: 35, name: "комедия"},
+    {id: 80, name: "криминал"},
+    {id: 99, name: "документальный"},
+    {id: 18, name: "драма"},
+    {id: 10751, name: "семейный"},
+    {id: 14, name: "фэнтези"},
+    {id: 36, name: "история"},
+    {id: 27, name: "ужасы"},
+    {id: 10402, name: "музыка"},
+    {id: 9648, name: "детектив"},
+    {id: 10749, name: "мелодрама"},
+    {id: 878, name: "фантастика"},
+    {id: 10770, name: "телевизионный фильм"},
+    {id: 53, name: "триллер"},
+    {id: 10752, name: "военный"},
+    {id: 37, name: "вестерн"}
+  ]
+
   popularFilms: PopularFilms[] = []
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.loadFilms();
+  }
+
+  loadFilms() {
     this.http.get<PopularFilms>(`https://api.themoviedb.org/3/movie/popular?api_key=b3097ca6783d35649a9f47c87dcbbfa0&language=ru-RU&page=1`)
-      .subscribe(response => {
-        this.popularFilms = response.results;
-      })
+    .subscribe(response => {
+      response.results.map((film: any) => {
+        film.genre_ids.map((number: number, i: number) => {
+          let genreArr = this.genres.find(genre => number === genre.id);
+          if (genreArr) {
+            film.genre_ids[i] = genreArr.name;
+          }
+        });
+        film.genre_ids = film.genre_ids.join(', ');
+      });
+      this.popularFilms = response.results;
+    })
   }
 
 }
