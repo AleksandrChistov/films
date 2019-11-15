826d85da84c61ca6efd126b44e525955b2c92e5f
AleksandrChistov <aleksandrchistov.ru@yandex.ru>
2019-11-15 16:51:38 +0700

    Передал id в компонент film и получил данные по id от сервера

--- src/app/film/film.component.ts
+++ src/app/film/film.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-film',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.http.get(`https://api.themoviedb.org/3/movie/${params.id}?api_key=b3097ca6783d35649a9f47c87dcbbfa0&language=ru-RU&append_to_response=recommendations%2Csimilar%2Cvideos`)
+        .subscribe(response => {
+          console.log(response);
+        });
+    })
   }
 
 }
